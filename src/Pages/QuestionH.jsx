da40f59c60ad9a15b899e0aead5aa59502ac6159
Afnaanshaalle110 <luulabdullahishaalle@gmail.com>
2024-07-07 13:14:02 -0700

    Quiz.jsx

--- src/Pages/QuestionH.jsx
+++ src/Pages/QuestionH.jsx
@@ -1,7 +1,55 @@
- const Questions = [
+export const Questions = [
     { 
+        prompt: "who is the last of the prophets and messengers peace be upon him ?",
+        opA: "Isse",
+        opB: "Ayub",
+        opC: "Yusuf",
+        opD: "Mohammed",
+        Answer: "opD",
      
-    }
-]
+    },
 
-export default Questions
+
+    { 
+        prompt: "who is the last of the prophets and messengers peace be upon him ?",
+        opA: "Isse",
+        opB: "Ayub",
+        opC: "Yusuf",
+        opD: "Mohammed",
+        Answer: "opD",
+     
+    },
+
+
+    { 
+        prompt: "who is the last of the prophets and messengers peace be upon him ?",
+        opA: "Isse",
+        opB: "Ayub",
+        opC: "Yusuf",
+        opD: "Mohammed",
+        Answer: "opD",
+     
+    },
+
+    { 
+        prompt: "who is the last of the prophets and messengers peace be upon him ?",
+        opA: "Isse",
+        opB: "Ayub",
+        opC: "Yusuf",
+        opD: "Mohammed",
+        Answer: "opD",
+     
+    },
+    { 
+        prompt: "who is the last of the prophets and messengers peace be upon him ?",
+        opA: "Isse",
+        opB: "Ayub",
+        opC: "Yusuf",
+        opD: "Mohammed",
+        Answer: "opD",
+     
+    },
+
+    
+];
+
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Questions } from '../Pages/QuestionH';
 
 const Quiz = () =>{
+    const [CurrQuestion, setCurrQuestion] = useState(0); 
+    const [optionchosen, setoptionchosen] = useState();
     return (
-        <div>Quiz</div>
+        <div className='Quiz'>
+            <h1>{Questions[CurrQuestion] .prompt}</h1>
+
+            <div className='opitions'>
+                <button> {Questions[CurrQuestion] .opA}</button>
+                <button> {Questions[CurrQuestion] .opB}</button>
+                <button> {Questions[CurrQuestion] .opC}</button>
+                <button> {Questions[CurrQuestion] .opD}</button>
+            
+            </div>
+            <button>Next</button>
+        </div>
         
     )
 }
