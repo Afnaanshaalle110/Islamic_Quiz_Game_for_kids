b81a7de32fac388d036b92f29a59ba553713def8
Afnaanshaalle110 <luulabdullahishaalle@gmail.com>
2024-07-07 14:13:00 -0700

    QuestionH.jsx

--- src/Pages/QuestionH.jsx
+++ src/Pages/QuestionH.jsx
@@ -1,52 +1,72 @@
 export const Questions = [
     { 
-        prompt: "who is the last of the prophets and messengers peace be upon him ?",
-        opA: "Isse",
+        prompt: "Who is the Seal of the Prophets?",
+        opA: "Ibrahim,",
         opB: "Ayub",
-        opC: "Yusuf",
-        opD: "Mohammed",
+        opC: "Isse",
+        opD: "Muhammad, may God bless him and grant him peace",
         Answer: "opD",
      
     },
 
 
     { 
-        prompt: "who is the last of the prophets and messengers peace be upon him ?",
-        opA: "Isse",
-        opB: "Ayub",
-        opC: "Yusuf",
-        opD: "Mohammed",
-        Answer: "opD",
+        prompt: "The prayer that has no voluntary voluntary prayers after it ?",
+        opA: "Al-fajr praye ",
+        opB: "Asr prayer",
+        opC: "Salat Aduher",
+        opD: "None",
+        Answer: "opB",
      
     },
 
 
     { 
-        prompt: "who is the last of the prophets and messengers peace be upon him ?",
-        opA: "Isse",
-        opB: "Ayub",
-        opC: "Yusuf",
-        opD: "Mohammed",
-        Answer: "opD",
+        prompt: "Who is the prophet who was called the Word of God?",
+        opA: "Prophet Muhammad, peace be upon him",
+        opB: "Adam",
+        opC: "Musse cs",
+        opD: "Isse",
+        Answer: "opC",
      
     },
 
     { 
-        prompt: "who is the last of the prophets and messengers peace be upon him ?",
-        opA: "Isse",
-        opB: "Ayub",
-        opC: "Yusuf",
-        opD: "Mohammed",
+        prompt: "How many daily prayers are there in Islam?",
+        opA: "1",
+        opB: "4",
+        opC: "3",
+        opD: "5",
         Answer: "opD",
      
     },
     { 
-        prompt: "who is the last of the prophets and messengers peace be upon him ?",
-        opA: "Isse",
-        opB: "Ayub",
-        opC: "Yusuf",
-        opD: "Mohammed",
-        Answer: "opD",
+        prompt: "what day is the Muslims special day?",
+        opA: "Sunday",
+        opB: "Friday",
+        opC: "Tuesday",
+        opD: "Wednesday",
+        Answer: "opB",
+     
+    },
+
+    { 
+        prompt: "How many names does Allah have?",
+        opA: "99",
+        opB: "612",
+        opC: "3",
+        opD: "1",
+        Answer: "opA",
+     
+    },
+
+    { 
+        prompt: "What is the surah without which prayer is not valid?",
+        opA: "Surah Al-Falaq.",
+        opB: "Surat Al-Asr",
+        opC: "Surah Al-Fatihah ",
+        opD: "None",
+        Answer: "opC",
      
     },
 
