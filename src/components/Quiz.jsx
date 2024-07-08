072c192a56bc155299e56ced34b73af085ad6a3c
Afnaanshaalle110 <luulabdullahishaalle@gmail.com>
2024-07-08 02:30:26 -0700

    App.jsx Quiz.jsx index,js

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,8 +4,8 @@ import { Questions } from '../Pages/QuestionH'
 
 const Quiz = () => {
     //   function update
-    const [score, setscore] = useContext(QuizContext); 
-    const [CurrQuestion, setCurrQuestion] = useState(0); 
+    const [score, setscore] = useContext(QuizContext);
+    const [CurrQuestion, setCurrQuestion] = useState(0);
     const [optionchosen, setoptionchosen] = useState("")
           // #netQuestion function
     const next = () => {
@@ -15,26 +15,21 @@ const Quiz = () => {
         alert(score);
         setCurrQuestion(CurrQuestion + 1);
     }
-    
-
     return (
         <div className='Quiz'>
             <h1>{Questions[CurrQuestion] .prompt}</h1>
             <div className='opitions'>
-                <button onClick={() => setoptionchosen("A")}> 
-                    {Questions[CurrQuestion] .opA}{""}</button>
-                <button onClick={() => setoptionchosen("B")}> 
-                    {Questions[CurrQuestion] .opB}{""}</button>
-                <button onClick={() => setoptionchosen("C")}>
-                     {Questions[CurrQuestion] .opC}{""}</button>
-                <button onClick={() => setoptionchosen("D")}> 
-                    {Questions[CurrQuestion] .opD}{""}</button>
+                <button onClick={() => setoptionchosen("A")} > 
+                    {Questions[CurrQuestion].opA}{""}</button>
+                <button onClick={() => setoptionchosen("B")} > 
+                    {Questions[CurrQuestion].opB}{""}</button>
+                <button onClick={() => setoptionchosen("C")} >
+                     {Questions[CurrQuestion].opC}{""}</button>
+                <button onClick={() => setoptionchosen("D")} > 
+                    {Questions[CurrQuestion].opD}{""}</button>
             </div>
             <button onClick={next}>Next</button>
         </div>
-        
-    )
-
+    );
 }
-
-export default Quiz;
+export default Quiz
